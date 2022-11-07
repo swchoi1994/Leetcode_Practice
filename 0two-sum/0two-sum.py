14d4c0f41676733fc1ea93664103205d979b37cf
Seongwoo Choi <swchoi1994@gmail.com>
2022-11-06 18:50:04 -0800

    Time: 67 ms, Memory: 15.1 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
+        seen_map = {}
         
         for i, num in enumerate(nums):
-            if target - num in hashmap:
-                return [hashmap[target - num], i]
-            hashmap[num] = i
-                
+            if target - num in seen_map:
+                return [seen_map[target - num], i]
+            seen_map[num] = i
