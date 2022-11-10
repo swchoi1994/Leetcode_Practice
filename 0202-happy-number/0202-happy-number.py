9d8c8faa63a99a020b7e24282ffec53caa875924
Seongwoo Choi <swchoi1994@gmail.com>
2022-11-10 10:16:51 -0800

    Time: 65 ms (53.15%), Space: 13.8 MB (97.05%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,26 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        visit = set()
-        
-        while n not in visit:
-            visit.add(n)
-            n = self.sumOfSquares(n)
-            
-            if n == 1:
-                return True
-        return False
-            
-    
-    def sumOfSquares(self, n:int) -> int:
-        # TODO:
-        # 19 % 10 = 9
-        # 19 / 10 = 1
-        
-        output = 0
-        
-        while n:
-            digit = n % 10
-            digit = digit ** 2
-            output += digit
-            n = n // 10
-        return output
+
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+
+        return n == 1
