591e1641914b26acbc9e71a610f7481225c4fa77
Seongwoo Choi <swchoi1994@gmail.com>
2022-11-09 18:12:19 -0800

    Time: 57 ms (58.54%), Space: 13.9 MB (19.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # need to use tabulation
+        if not nums:
+            return 0
+        if len(nums) <= 2:
+            return max(nums)
+        
         rob1, rob2 = 0, 0
         
         for n in nums:
